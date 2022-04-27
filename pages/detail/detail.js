78e39253f011b17babe9a17d9db539a0f304f754
heng1025 <472263356@qq.com>
2022-04-28 07:37:09 +0800

    feat: add audio play

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,29 +1,73 @@
-import { getNewsById } from "@1r21/api-wx";
-import { parseText } from "@1r21/util";
+import { getNewsById, translate as t } from "@1r21/api-wx";
+import { parseText, Translation } from "@1r21/util";
+
 
 Page({
   data: {
     nodes: [],
+    paused: true,
   },
   async onLoad(option) {
-    const { id } = option || {};
-    const { transcript, date } = await getNewsById(id)
-    const nodes = parseText(transcript)
+    const { id = 410 } = option || {};
+    const { transcript, date, src, cover } = await getNewsById(id);
+    const nodes = parseText(transcript);
     this.setData({
-      nodes
+      nodes,
+      cover,
     });
     wx.setNavigationBarTitle({
-      title: date
-    })
-  },
+      title: date,
+    });
+    const innerAudioContext = wx.createInnerAudioContext();
+    innerAudioContext.src = src;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    innerAudioContext.onPlay(() => {
+      console.log("start play");
+    });
 
-  },
-  playHandle() {
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg);
+      console.log(res.errCode);
+    });
 
+    this.innerAudioContext = innerAudioContext;
   },
-})
+  onUnload() {
+    this.innerAudioContext.stop()
+  },
+  play() {
+    this.setData({
+      paused: false,
+    });
+    this.innerAudioContext.play();
+  },
+  pause() {
+    this.setData({
+      paused: true,
+    });
+    this.innerAudioContext.pause();
+  },
+  goBack() {
+    wx.navigateTo({
+      url: `/pages/index/index`,
+    });
+  },
+  async doTranslate(event) {
+    const { dataset } = event.currentTarget
+    const { text } = dataset
+    const texts = this.data.nodes;
+    if (!text.trans) {
+      const { list = [] } = await t(text.value);
+      if (list && list.length) {
+        const [first] = list
+        const updateNodes = texts.map(v => ({
+          ...v,
+          trans: v.type === 'text' && v.value === first.src ? first.dst : ''
+        }))
+        this.setData({
+          nodes: updateNodes
+        })
+      }
+    }
+  },
+});
